70757fa2d0a0351649aa286833b0358dcc982594
MOHIT-JANGIR <61896468+MOHIT-JANGIR@users.noreply.github.com>
2021-06-28 17:24:55 +0530

    Update update.py

--- K8S-WS/update.py
+++ K8S-WS/update.py
@@ -1,12 +1,35 @@
-def pv_update(storage):
+def pv_update(pv_file,volume_id,storage):
   import yaml
 
-  file_name = "pv-ebs.yml"
+  # file_name = "pv-ebs.yml"
 
-  stream = open(file_name, 'r')
+  stream = open(pv_file, 'r')
   data = yaml.load(stream,Loader=yaml.FullLoader)
 
   data['spec']['capacity']['storage'] = str(storage) + 'Gi'
+  data['spec']['awsElasticBlockStore']['volumeID'] = volume_id
 
-  with open(file_name, 'w') as yaml_file:
+  with open(pv_file, 'w') as yaml_file:
      yaml_file.write( yaml.dump(data, default_flow_style=False))
+
+
+def pvc_update(pvc_file,storage):
+  import yaml
+
+  # file_name = "pvc-ebs.yml"
+
+  stream = open(pvc_file, 'r')
+  data = yaml.load(stream,Loader=yaml.FullLoader)
+
+  data['spec']['resources']['requests']['storage'] = str(storage) + 'Gi'
+
+  with open(pvc_file, 'w') as yaml_file:
+     yaml_file.write( yaml.dump(data, default_flow_style=False))
+
+
+'''
+volume_id=subprocess.getoutput("""aws ec2 describe-volumes --filters Name=tag-value,Values="Terraform-Value" --query "Volumes[0].VolumeId" """)
+
+update("pv-ebs.yml",volume_id,5)
+update("pvc-ebs.yml",5)
+'''
